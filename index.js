2696c95edd66f6353aaf30462056e5c73e0d7dcf
Michael Swanson <mswanson@1524interactive.com>
2018-04-09 23:37:16 -0500

    cleanup, consistent variable names and configuration option maps

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-sass-atoms'
+  name: 'ember-sass-atoms',
+
+  included: function(/* app */) {
+    this._super.included.apply(this, arguments);
+  },
+
+  isDevelopingAddon() {
+    return true;
+  }
 };
